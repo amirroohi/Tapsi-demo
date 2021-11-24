089004415a9982b3bf9ba059c20384198725be84
amirroohi <amirroohi@yahoo.com>
2021-11-25 01:01:25 +0330

    all files added

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// NAVBAR
+const toggler = document.querySelector(".nav__toggler");
+const navbar = document.querySelector(".nav");
+toggler.addEventListener("click", () => {
+  navbar.classList.toggle("nav__expanded");
+});
+// TABS
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", (e) => {
+    const targetTabContent = document.querySelector(
+      `#${tab.dataset.tabTarget}`
+    );
+    // remove pre state
+    tabs.forEach((tab) => tab.classList.remove("active"));
+    tabContents.forEach((tabContent) => tabContent.classList.remove("active"));
+    // add new state
+    tab.classList.add("active");
+    targetTabContent.classList.add("active");
+  });
+});
